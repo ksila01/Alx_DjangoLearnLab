f148808a77b7cff49dafd23a34f8d39f48d29a88
Sila <ksila04@gmail.com>
2025-08-14 20:17:34 +0300

    revised task_1

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     path('books/', BookListView.as_view(), name='book-list'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
     path('books/create/', BookCreateView.as_view(), name='book-create'),
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
+    path('books/<int:pk>/update', BookUpdateView.as_view(), name='book-update'),  # removed trailing slash
+    path('books/<int:pk>/delete', BookDeleteView.as_view(), name='book-delete'),  # removed trailing slash
 ]
