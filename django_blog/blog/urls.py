6dd1e25b3677c919a72cd32bd7e37e71386f603d
Sila <ksila04@gmail.com>
2025-08-17 20:16:30 +0300

    task-2-revision

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -5,10 +5,10 @@ urlpatterns = [
     # Task 0: Index
     path('', views.index, name='index'),
 
-    # Task 3: Blog post CRUD
-    path('posts/', views.PostListView.as_view(), name='post_list'),  # List all posts
-    path('posts/new/', views.PostCreateView.as_view(), name='post_create'),  # Create new post
-    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),  # View post details
-    path('posts/<int:pk>/update/', views.PostUpdateView.as_view(), name='post_update'),  # Edit post
-    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),  # Delete post
+    # Task 3: Blog post CRUD (singular 'post/')
+    path('post/', views.PostListView.as_view(), name='post_list'),  # List all posts
+    path('post/new/', views.PostCreateView.as_view(), name='post_create'),  # Create new post
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),  # View post details
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post_update'),  # Edit post
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),  # Delete post
 ]
