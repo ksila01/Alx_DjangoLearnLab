d03b8103fd2b05be66e222cbbcb1fbc5b4a5f04e
Sila <ksila04@gmail.com>
2025-08-17 19:27:12 +0300

    task_1_committed

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,4 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import CustomUserCreationForm  # Make sure forms.py exists
 
+# ---------- Task 0 view ----------
 def index(request):
     return render(request, 'blog/index.html')
+
+# Registration view
+def register_view(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful.")
+            return redirect("profile")
+        else:
+            messages.error(request, "Unsuccessful registration. Invalid information.")
+    else:
+        form = CustomUserCreationForm()
+    return render(request, "blog/register.html", {"form": form})
+
+# Login view
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("profile")
+        else:
+            messages.error(request, "Invalid username or password.")
+    else:
+        form = AuthenticationForm()
+    return render(request, "blog/login.html", {"form": form})
+
+# Logout view
+@login_required
+def logout_view(request):
+    logout(request)
+    messages.info(request, "Logged out successfully!")
+    return redirect("login")
+
+# Profile view
+@login_required
+def profile_view(request):
+    if request.method == "POST":
+        user = request.user
+        user.email = request.POST.get("email")
+        user.save()
+        messages.success(request, "Profile updated successfully.")
+    return render(request, "blog/profile.html")
